f0f0535515ab1c93a1846b81307c245918857ef4
Umar Hasan <umar.b.hasan@outlook.com>
2024-12-09 00:43:01 +0600

    Target class frequency after SMOTE

--- classifiers.py
+++ classifiers.py
@@ -369,3 +369,13 @@ print(pd.Series(y_train).value_counts())
 # Check class distribution
 print("Class distribution in testing set after SMOTE:")
 print(pd.Series(y_test).value_counts())
+
+# Plotting
+plt.figure(figsize=(10, 6))
+sns.countplot(x=y_resampled)
+plt.title('Target Class Distribution after SMOTE')
+plt.xlabel('Fatal Casualty Type')
+plt.ylabel('Frequency')
+plt.xticks(rotation=45, ha='right')
+plt.tight_layout()
+plt.show()
